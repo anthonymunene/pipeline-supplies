5aacf575cee6c487090713eab4e287b261b4fbed
Anthony Munene <munene.anthony@gmail.com>
2014-02-10 15:55:29 +0000

    am added sass files

---
+++ wp-content/themes/modernist/scss/config-production.rb
@@ -0,0 +1,10 @@
+require "susy"
+http_path = "/"
+http_images_path = "/content/themes/hssc/"
+css_dir = "dist/css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
---
+++ wp-content/themes/modernist/scss/config.rb
@@ -0,0 +1,10 @@
+require "susy"
+http_path = "/"
+http_images_path = "/content/themes/modernist/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :expanded
+relative_assets=true
+line_comments = false
